01c9ddb8da73ae53c37e788b1123d5f46af1e506
Maxim Golovanev <maxim.golovanev@liflandgaming.com>
2021-01-20 16:11:33 +0300

    Ad errors logging

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,9 +1,12 @@
 import { ErrorRequestHandler } from 'express';
+import logger from '../util/logger';
 
-const errorHandler: ErrorRequestHandler = function (err, req, res) {
+const errorHandler: ErrorRequestHandler = function (err, req, res, _next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  logger.error(err.message);
 
   // render the error page
   res.status(err.status || 500);
